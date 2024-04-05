68236747c84995610fae344deeab62446dde9674
Robin <r.velden@cbyte.nl>
2024-04-05 12:56:15 +0200

    Added changes for own purposes

--- android/src/main/kotlin/com/incrediblezayed/file_saver/Dialog.kt
+++ android/src/main/kotlin/com/incrediblezayed/file_saver/Dialog.kt
@@ -20,6 +20,8 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import java.io.File
 import java.io.OutputStream
+import java.io.FileInputStream
+import java.io.FileOutputStream
 
 
 private const val SAVE_FILE = 886325063
@@ -28,6 +30,7 @@ class Dialog(private val activity: Activity) : PluginRegistry.ActivityResultList
     private var result: MethodChannel.Result? = null
     private var bytes: ByteArray? = null
     private var fileName: String? = null
+    private var cachePath: String? = null
     private val TAG = "Dialog Activity"
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?): Boolean {
@@ -48,23 +51,25 @@ class Dialog(private val activity: Activity) : PluginRegistry.ActivityResultList
     }
 
     fun openFileManager(
-        fileName: String?,
-        ext: String?,
-        bytes: ByteArray?,
-        type: String?,
-        result: MethodChannel.Result
+            fileName: String?,
+            ext: String?,
+            bytes: ByteArray?,
+            type: String?,
+            result: MethodChannel.Result
     ) {
         Log.d(TAG, "Opening File Manager")
         this.result = result
         this.bytes = bytes
-        this.fileName = fileName
+        this.cachePath = fileName
+        this.fileName = "easyperformer_export"
+
         val intent =
-            Intent(Intent.ACTION_CREATE_DOCUMENT)
+                Intent(Intent.ACTION_CREATE_DOCUMENT)
         intent.addCategory(Intent.CATEGORY_OPENABLE)
-        intent.putExtra(Intent.EXTRA_TITLE, "$fileName.$ext")
+        intent.putExtra(Intent.EXTRA_TITLE, "easyperformer_export.$ext")
         intent.putExtra(
-            DocumentsContract.EXTRA_INITIAL_URI,
-            Environment.getExternalStorageDirectory().path
+                DocumentsContract.EXTRA_INITIAL_URI,
+                Environment.getExternalStorageDirectory().path
         )
         intent.type = type
         activity.startActivityForResult(intent, SAVE_FILE)
@@ -92,14 +97,26 @@ class Dialog(private val activity: Activity) : PluginRegistry.ActivityResultList
     }
 
     private fun saveFile(uri: Uri) {
+        val sourceFile = File(this.cachePath) // Assuming this.cachePath is your cache file path
+        val inputStream = FileInputStream(sourceFile)
+
+        // Create output stream to destination file
+        val opStream = activity.contentResolver.openOutputStream(uri)
+
+        // Buffer for reading/writing data
+        val buffer = ByteArray(1024)
+        var length: Int
+
         try {
-            Log.d(TAG, "Saving file")
-
-            val opStream = activity.contentResolver.openOutputStream(uri)
-            opStream?.write(bytes)
-
-        } catch (e: Exception) {
-            Log.d(TAG, "Error while writing file" + e.message)
+            opStream?.use { output ->
+                // Read from input stream and write to output stream in chunks
+                while (inputStream.read(buffer).also { length = it } > 0) {
+                    output.write(buffer, 0, length)
+                }
+            }
+        } finally {
+            // Close the input stream
+            inputStream.close()
         }
     }
 }
